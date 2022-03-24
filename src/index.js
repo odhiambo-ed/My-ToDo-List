b5fa6d09b32797dd36b0582a377dc0ac08439ef8
Edward Odhiambo <odhiambo.edward111@gmail.com>
2022-03-25 00:50:13 +0300

    Edit Index js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import _ from "lodash";
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+import getAllTodos from './modules/getTodos.js';
+import Todo from './modules/todo.js';
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const initialArr = [
+  {
+    todo: 'add water',
+    completed: false,
+  },
+  {
+    todo: 'wash dishes',
+    completed: false,
+  },
+];
 
-  return element;
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const getTodos = JSON.parse(localStorage.getItem('todos'));
+  if (getTodos === null || getTodos.length === 0) {
+    localStorage.setItem('todos', JSON.stringify(initialArr));
+    getAllTodos();
+  } else {
+    getAllTodos();
+  }
+});
 
-document.body.appendChild(component());
+const todoList = new Todo();
+
+document.querySelector('#todo-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const todoItem = document.querySelector('#todo-input');
+  todoList.addTodo(todoItem.value);
+  todoItem.value = '';
+});
+
+const clearCompleted = document.querySelector('#clear-title');
+clearCompleted.addEventListener('click', () => {
+  const existingTodos = JSON.parse(localStorage.getItem('todos'));
+  const correctTodos = existingTodos.filter((val) => !val.completed);
+  localStorage.setItem('todos', JSON.stringify(correctTodos));
+  window.location.reload(true);
+});
